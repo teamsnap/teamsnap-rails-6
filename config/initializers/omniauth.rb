996cfab10d90e93de8611b9b11415c647331749d
Ryan W <rwilliamz@gmail.com>
2019-08-24 15:15:13 -0700

    More for CVE-2015-9284

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,4 +3,3 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :teamsnap, ENV['TEAMSNAP_CLIENT_ID'], ENV['TEAMSNAP_CLIENT_SECRET'], scope: "read write"
 end
 OmniAuth.config.logger = Rails.logger
-OmniAuth.config.allowed_request_methods = [:post, :get]
